cfadc94eae2fd988a3b06a175a09222872a93d45
keangkai-kp <keangkai.p@appman.co.th>
2020-11-09 02:27:41 +0700

    feat: add bootstrap in header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+//component
+import Header from './components/Header'
+import Footer from './components/Footer'
+//bootstrap
+import { Container } from "react-bootstrap"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+      <main className="py-3">
+        <Container>
+          <h1>WElcome to proshop</h1>
+        </Container>
+      </main>
+    <Footer />
+    </>
   );
 }
 
