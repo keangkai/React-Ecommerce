e248b1415580d64727d0a9cf90f3d2d056f1d0a4
Pornpawee Srithanasarn <pornpaweesrithanasarn@MacBook-Pro-khxng-Pornpawee.local>
2020-12-03 23:02:49 +0700

    feat: add redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Row,Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from "react-redux"
 import Product from '../components/Product'
-import axios from "axios"
-// import products from "../products"
+import { listProducts } from "../actions/productActions"
+import Message from "../components/Message"
+import Loader from "../components/Loading"
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('http://localhost:5000/products')
 
-            setProducts(data)
-        }
+        dispatch(listProducts())
 
-        fetchProducts()
-    }, [])
+    }, [dispatch])
+
 
     return (
         <>
          <h1>Latest Product</h1>
-            <Row>
-                {
-                    products.map((product, idx) => (
-                        <Col key={product._id} sm={12} md={6} lg={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))
-                }
-            </Row>   
+         { loading ? 
+            <Loader /> : 
+         error ? 
+            <Message variant="danger">{error}</Message> : 
+         <Row>
+         {
+             products.map((product, idx) => (
+                 <Col key={product._id} sm={12} md={6} lg={3}>
+                     <Product product={product} />
+                 </Col>
+             ))
+         }
+        </Row>   
+         }
+            
         </>
     )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
+import { productListReducer, productDetailReducer } from "./reducers/productReducer.js"
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailReducer
+})
 
 const initialState = {}
 
