6a8b77f41c4fe510098d509fbde2538034093bf3
Pornpawee Srithanasarn <pornpaweesrithanasarn@MacBook-Pro-khxng-Pornpawee.local>
2020-12-17 07:58:37 +0700

    feat: update shopping cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,19 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 import { productListReducer, productDetailReducer } from "./reducers/productReducer.js"
+import { cartReducer } from "./reducers/cartReducer.js"
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailReducer
+    productDetails: productDetailReducer,
+    cart: cartReducer
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage }
+}
 
 const middleware = [thunk]
 
