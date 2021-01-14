033acc16591c5f1946cd1f7fc4886babcad103c6
Keangkai <keangkai@Keangkais-MacBook-Pro.local>
2021-01-14 19:31:02 +0700

    feat: update login register and get profile api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,16 +25,64 @@ const authUser = asyncHamdler(async(req, res) => {
     }
 })
 
+// @desc   Register a new user
+// @route  POST /api/users
+// @access PRIVATE
+
+const registerUser = asyncHamdler(async(req, res) => {
+    const { name, email, password } = req.body
+
+    const userExist = await User.findOne({ email })
+
+    if (userExist) {
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+})
+
 // @desc   Get user profile
 // @route  GET /api/users/profile
 // @access PUBLIC
 
 const getUser = asyncHamdler(async(req, res) => {
 
-    // const user = await User.findById({ email })
-    res.send('Success')
+    const user = await User.findById(req.user._id)
 
+    // console.log('user', user)
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+    // res.send('Success')
 
 })
 
-export { authUser, getUser }
+
+
+export { authUser, getUser, registerUser }
